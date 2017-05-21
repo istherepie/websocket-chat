5ace3919a2773dcc2ae654145a1fe206c86d84cd
Steffen Park <dev@istherepie.com>
2017-05-21 23:59:18 +0200

    Import project files

---
+++ server.js
@@ -0,0 +1,41 @@
+'use strict'
+
+// Modules
+const http = require('http')
+const express = require('express')
+const WebSocket = require('ws')
+
+// Constants
+const app = express()
+const server = http.createServer(app)
+const port = process.env.SERVER_PORT || 4000
+const wss = new WebSocket.Server({ server })
+
+// Clients map
+let clients = new Map()
+
+// Websocket
+wss.on('connection', (socket, request) => {
+	let key = request.headers['sec-websocket-key']
+  clients.set(key, socket)
+
+  socket.on('message', message => {
+    clients.forEach(client => {
+    	client.send(message)
+    })
+  })
+
+  socket.send('Welcome to the chat')
+
+  socket.on('close', event => {
+  	clients.delete(key)
+  })
+})
+
+// Serve static content
+app.use('/', express.static('public'))
+
+// Start server
+server.listen(port, function() {
+	console.log('Server is fully armed and operational on port: %s', port)
+})
