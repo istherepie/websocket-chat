45320c7646f2b9612a39a5e0c29bab4170bea90e
Steffen Park <dev@istherepie.com>
2017-06-12 02:09:27 +0200

    Added VueJS to views

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,50 +1,51 @@
 import Vue from 'vue'
 
-// if (!window.WebSocket) {
-//   let error = document.createElement('p')
-//   error.innerHTML = 'Sorry, your browser does not support websockets'
-//   messages.append(error)
-// }
-
 new Vue({
   el: '#root',
   data: {
-    //
+    notSupported: false,
+    username: null,
+    input: null,
+    messages: [],
+    socket: null
   },
   methods: {
-    //
+    sendMessage() {
+      this.socket.send(this.input)
+      this.input = null
+    },
+    removeMessage() {
+      this.messages.splice(0, 1)
+    }
   },
   mounted() {
-    // If browser supports websockets
-     if (window.WebSocket) {
-      // Set protocol
-      let protocol = 'wss://'
-
-      if (location.protocol != 'https:') {
+    // Set protocol
+    let protocol = 'wss://'
+    
+    if (location.protocol != 'https:') {
         protocol = 'ws://'
       }
 
-      // Create WebSocket connection.
-      const socket = new WebSocket(protocol + location.host)
+    if (!window.WebSocket) {
+      this.notSupported = true
+    }
+  
+    // Create WebSocket connection.
+    const socket = new WebSocket(protocol + location.host)
 
       // Connection opened
-      socket.addEventListener('open', function (event) {
+      socket.addEventListener('open', event => {
         console.log('Websocket connection open')
+        this.socket = socket
       })
 
       // Listen for messages
-      socket.addEventListener('message', function (event) {
-        let incoming = document.createElement('p')
-        incoming.innerHTML = event.data
-        messages.append(incoming)
-      })
-
-      // Send input value when clicked
-      submit.addEventListener('click', event => {
-        let message = input.value
-        socket.send(message)
-        input.value = null
+      socket.addEventListener('message', event => {
+        if (this.messages.length >= 10) {
+          this.removeMessage()
+        }
+        console.log('NEW MESSAGE: ' + event)
+        this.messages.push(event.data)
       })
     }
-  }
 })
